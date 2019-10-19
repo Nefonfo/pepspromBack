837058a7aa14ce62fc46e4a6c2487423bcf385a2
mac <mac@MacBook-Pro-de-mac.local>
2019-10-19 12:18:09 -0500

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SheetsStorageModule } from './routes/sheets-storage/sheets-storage.module';
 import { StorageInfoModule } from './routes/storage-info/storage-info.module';
-//mongodb+srv://Nefonfo:Kirisaki5501@pepsprops-5tfr0.mongodb.net/pepscont?retryWrites=true&w=majority
+//
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/pepscont', {
+    MongooseModule.forRoot('mongodb+srv://Nefonfo:Kirisaki5501@pepsprops-5tfr0.mongodb.net/contabilidad?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }),
